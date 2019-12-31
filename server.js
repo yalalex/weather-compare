653ad37af6774897ab6de1a1627a79491d4c5461
unknown <ayalouskikh@elinext.com>
2019-12-31 14:33:49 +0300

    connected DB in server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('');
+const connectDB = require('./config/db');
 
 const app = express();
+
+connectDB();
 
 app.get('/', (req, res) => res.json({ msg: 'Welcome to Weather Compare' }));
 
