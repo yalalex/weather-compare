f37f6c23baf46b0a3a58fba2cb5fbda352f06e0c
unknown <ayalouskikh@elinext.com>
2020-01-04 17:37:27 +0300

    added swtich to the Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,72 @@
-import React from 'react';
-import { AppBar } from '@material-ui/core';
+import React, { useState } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Switch from '@material-ui/core/Switch';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+const AntSwitch = withStyles(theme => ({
+  root: {
+    width: 28,
+    height: 16,
+    padding: 0,
+    display: 'flex'
+  },
+  switchBase: {
+    padding: 2,
+    color: theme.palette.grey[500],
+    '&$checked': {
+      transform: 'translateX(12px)',
+      color: theme.palette.common.white,
+      '& + $track': {
+        opacity: 1,
+        backgroundColor: theme.palette.primary.main,
+        borderColor: theme.palette.primary.main
+      }
+    }
+  },
+  thumb: {
+    width: 12,
+    height: 12,
+    boxShadow: 'none'
+  },
+  track: {
+    border: `1px solid ${theme.palette.grey[500]}`,
+    borderRadius: 16 / 2,
+    opacity: 1,
+    backgroundColor: theme.palette.common.white
+  },
+  checked: {}
+}))(Switch);
 
 export const Header = () => {
+  const [state, setState] = useState({
+    checked: true
+  });
+
+  const handleChange = name => event => {
+    setState({ ...state, [name]: event.target.checked });
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton
-          edge="start"
-          className={classes.menuButton}
-          color="inherit"
-          aria-label="menu"
-        >
-          <MenuIcon />
-        </IconButton>
         <Typography variant="h6" className={classes.title}>
           Weather Compare
         </Typography>
-        <Button color="inherit">Imperial</Button>
+        <Typography component="div">
+          <Grid component="label" container alignItems="center" spacing={1}>
+            <Grid item>Off</Grid>
+            <Grid item>
+              <AntSwitch
+                checked={state.checked}
+                onChange={handleChange('checked')}
+                value="checkedC"
+              />
+            </Grid>
+            <Grid item>On</Grid>
+          </Grid>
+        </Typography>
       </Toolbar>
     </AppBar>
   );
