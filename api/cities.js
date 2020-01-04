d48e152dadc74cc0f390e38a0d3961de8dc4cb86
unknown <ayalouskikh@elinext.com>
2020-01-04 15:10:38 +0300

    added sf to cities list

--- api/cities.js
+++ api/cities.js
@@ -3,5 +3,6 @@ exports.cities = [
   { name: 'Kiev', lat: 50.45, lon: 30.52 },
   { name: 'Amsterdam', lat: 52.37, lon: 4.89 },
   { name: 'New York', lat: 40.71, lon: 74.01 },
-  { name: 'Los Angeles', lat: 34.05, lon: 118.24 }
+  { name: 'Los Angeles', lat: 34.05, lon: 118.24 },
+  { name: 'San Francisco', lat: 37.77, lon: 122.42 }
 ];
