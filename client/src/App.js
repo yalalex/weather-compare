701ed571515e6c59225eb089bd15f0251f5a46ce
AlexY <alexyo87@yahoo.com>
2020-01-10 01:01:20 +0300

    installed & activated materialize-css

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 import Header from './components/Header';
@@ -7,9 +7,17 @@ import WorldMap from './components/WorldMap';
 // import Daily from './components/Daily';
 import useSwitchUnits from './hooks/useSwitchUnits';
 
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
 const App = () => {
+  useEffect(() => {
+    M.AutoInit();
+  });
+
   const [units, switchUnits] = useSwitchUnits('metric');
   const { switchTemp, convertTemp } = switchUnits;
+
   return (
     <div className='App'>
       <Header units={units} switchTemp={switchTemp} />
